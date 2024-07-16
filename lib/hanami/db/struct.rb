fcfbd02051a7e3d5073d411a4ad3bb6e2453f78e
mrimp <krzysiekkamilpiotrek@gmail.com>
2024-07-16 10:27:40 +0200

    Add documentation

--- lib/hanami/db/struct.rb
+++ lib/hanami/db/struct.rb
@@ -5,7 +5,10 @@ module Hanami
     # @api public
     # @since 2.2.0
     class Struct < ROM::Struct
-
+      # @api public
+      # @since 2.2.0
+      #
+      # Simple conversion of attributes to JSON format, without this method, the instance of the struct gets converted to a string
       def to_json
         to_h.to_json
       end
